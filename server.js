06f0bea7cd231252afb2cdff324c29d2e7848eab
LesterEvSe <evgenii.nikolaevichfgh@gmail.com>
2024-10-30 18:15:25 +0200

    fix: delete unnecessary comments

--- server.js
+++ server.js
@@ -6,19 +6,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 
-app.use(bodyParser.json()); // Додаємо body-parser для парсингу JSON
+app.use(bodyParser.json());
 
 // Serve the index.html file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Обробник POST-запиту для отримання токена
 app.post('/token', (req, res) => {
   const { id_token } = req.body;
   console.log("Received id_token on server:", id_token);
-
-  // Можна повернути підтвердження клієнту
   res.json({ message: 'Token received', id_token });
 });
 
